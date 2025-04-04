889287609c112dbec4ca7454bd0a20d1d5b4f069
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-04-04 15:35:58 -0600

    Add button, add DOM elements to JS file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("hello!")
+// Grab Form Elements
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postal = document.getElementById("postal_code");
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirm");
+const submit = document.getElementById("submit");
