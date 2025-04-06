cea2cf56d5b362d5485dd2815e1f11a14c3e0b0b
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-04-06 15:50:25 -0600

    Add Email and Postal valdiation

--- scripts.js
+++ scripts.js
@@ -5,3 +5,66 @@ const postal = document.getElementById("postal_code");
 const password = document.getElementById("password");
 const confirm = document.getElementById("confirm");
 const submit = document.getElementById("submit");
+const form = document.getElementById("form");
+
+// Error Elements
+const emailError = document.getElementById("email-error");
+const countryError = document.getElementById("country-error");
+const postalError = document.getElementById("postal-error");
+const passwordError = document.getElementById("password-error");
+const confirmError = document.getElementById("confirm-error");
+
+// Email validation
+function showEmailError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "You need to enter an email address (e.g. user@example.com)";
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "Entered value needs to be an email address";
+    }
+    emailError.className = "error active";
+}
+
+email.addEventListener("blur", () => {
+    if (email.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = "error";
+    } else {
+        showEmailError();
+    }
+});
+
+email.addEventListener("input", () => {
+    if (email.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = "error";
+    }
+});
+
+// Country Validation
+
+
+// Postal Code Validation
+function showPostalError() {
+    if (postal.validity.valueMissing) {
+        postalError.textContent = "You need to enter a 5 digit postal code (e.g. 12345)"
+    } else if (postal.validity.patternMismatch) {
+        postalError.textContent = "Must be exactly 5 digits (no letters or symbols)"
+    }
+    postalError.className = "error active";
+}
+
+postal.addEventListener("blur", () => {
+    if (postal.validity.valid) {
+        postalError.textContent = '';
+        postalError.className = "error";
+    } else {
+        showPostalError();
+    }
+});
+
+postal.addEventListener("input", () => {
+    if (postal.validity.valid) {
+      postalError.textContent = "";
+      postalError.className= "error";
+    }
+});
