b1126e7ff7faaa018a74d00abcec1b244ebb80f3
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-04-06 18:13:36 -0600

    Add remaining validations, style error messages

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const email = document.getElementById("email");
 const country = document.getElementById("country");
 const postal = document.getElementById("postal_code");
 const password = document.getElementById("password");
-const confirm = document.getElementById("confirm");
+const confirmPassword = document.getElementById("confirm");
 const submit = document.getElementById("submit");
 const form = document.getElementById("form");
 
@@ -41,7 +41,28 @@ email.addEventListener("input", () => {
 });
 
 // Country Validation
+function showCountryError() {
+    if (country.validity.valueMissing) {
+        countryError.textContent = "Please select a country";
+    }
+    countryError.className = "error active";
+}
 
+country.addEventListener("blur", () => {
+    if (country.validity.valid) {
+        countryError.textContent = '';
+        countryError.className = "error";
+    } else {
+        showCountryError();
+    }
+});
+
+country.addEventListener("input", () => {
+    if (country.validity.valid) {
+        countryError.textContent = '';
+        countryError.className = "error";
+    }
+});
 
 // Postal Code Validation
 function showPostalError() {
@@ -66,5 +87,67 @@ postal.addEventListener("input", () => {
     if (postal.validity.valid) {
       postalError.textContent = "";
       postalError.className= "error";
+    }
+});
+
+// Password Validation
+function showPasswordError() {
+    if (password.validity.valueMissing) {
+        passwordError.textContent = "Please enter a password";
+        passwordError.className = "error active";
+    } else if (password.validity.tooShort) {
+        passwordError.textContent = "Password must be 8 characters or more";
+        passwordError.className = "error active";
+    }
+}
+
+password.addEventListener("blur", () => {
+    if (password.validity.valid) {
+        passwordError.textContent = '';
+        passwordError.className = "error";
+    } else {
+        showPasswordError();
+    }
+});
+
+password.addEventListener("input", () => {
+    if (password.validity.valid) {
+        passwordError.textContent = '';
+        passwordError.className = "error";
+    }
+});
+
+
+// Confirm Password Validation
+function confirmCheck() {
+    const passwordValue = document.getElementById("password").value;
+    const confirmValue = document.getElementById("confirm").value;
+    if (passwordValue !== confirmValue) {
+        confirmError.textContent = "Passwords do not match!";
+        confirmError.className = "error active";
+        confirmPassword.setCustomValidity("Passwords must match");
+    } else if (confirmValue === '') {
+        confirmError.textContent = "Please confirm your password";
+        confirmError.className = "error active";
+        confirmPassword.setCustomValidity("Password confirmation required");
+    } else if (confirmValue === passwordValue) {
+        confirmError.textContent = '';
+        confirmError.className = "error";
+        confirmPassword.setCustomValidity("");
+    }
+}
+
+confirmPassword.addEventListener("blur", confirmCheck);
+confirmPassword.addEventListener("input", confirmCheck);
+
+// Check if the whole form is valid, if so, give the user a cookie
+submit.addEventListener("click", (event) => {
+    if (form.reportValidity()) {
+        event.preventDefault();
+        alert("Nice job! You filled out the form correctly! 🖐️");
+    } else {
+        event.preventDefault();
+        const invalidField = form.querySelector(':invalid');
+        invalidField?.focus();
     }
 });
